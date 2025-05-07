a7a43e6d452faf0007b7d77ff3028e4f4ebd83d9
indra <ii5084652@gmail.com>
2025-05-07 09:44:12 -0700

    first commit added some changes and made some changes

---
+++ src/com/basic/Demo.java
@@ -0,0 +1,9 @@
+package com.basic;
+
+public class Demo {
+    public static void main(String[] args){
+        String channel = "Coders Archade";
+        String request = "Subsribe";
+        System.out.println("Please"+ request + " to our channel "+ channel);
+    }
+}
